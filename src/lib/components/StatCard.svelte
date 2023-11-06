98e7a5bd459aa598020463aa6cf9cc19b637e0d8
James Napier <jamesnapier64@gmail.com>
2023-11-06 19:38:28 +1300

    make mobile and dark friendly

---
+++ src/lib/components/StatCard.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let stat: number;
+	export let title: string;
+</script>
+
+<div
+	class="w-64 rounded-xl overflow-hidden shadow-md text-gray-700 dark:text-gray-400 bg-white dark:bg-gray-800"
+>
+	<div class="px-6 py-4">
+		<div class=" mb-2">{title}</div>
+		<p class="text-2xl font-bold text-black dark:text-white">
+			{stat.toFixed(2)}%
+		</p>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,24 +37,19 @@
 	}
 </script>
 
-<section>
-	<div class="text-center">
-		<h1
-			class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
-		>
-			What's my <span
-				class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-600"
-				>grade</span
-			>?
-		</h1>
-		<p
-			class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400"
-		>
-			A tool to help students calculate their current grades in various courses. You can input
-			assessment scores and weightings, and be provided with an estimate of your current standing in
-			the course.
-		</p>
-	</div>
+<section class="text-center">
+	<h1
+		class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+	>
+		What's my <span
+			class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-600">grade</span
+		>?
+	</h1>
+	<p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+		A tool to help students calculate their current grades in various courses. You can input
+		assessment scores and weightings, and be provided with an estimate of your current standing in
+		the course.
+	</p>
 </section>
 <section class=" py-5">
 	<div class="flex justify-center">
