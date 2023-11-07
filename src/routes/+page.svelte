9f1e214a65e4c481799a74cda17b5c0069fc813b
James Napier <jamesnapier64@gmail.com>
2023-11-07 19:13:01 +1300

    improve local storage loading behavior; fix table contents moving on hover

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,11 +9,13 @@
 	} from '$lib/grades';
 	import { validateAssessments, type Assessment } from '$lib/zod';
 
-	let assessments: Assessment[] = getLocalAssessments() || [
+	const DEMO_ASSESSMENTS = [
 		{ name: 'Exam', weight: 50.0, mark: 100.0, invigilated: false },
-		{ name: 'Assignment 2', weight: 25.0, mark: 100.0, invigilated: false },
-		{ name: 'Assignment 3', weight: 25.0, mark: 100.0, invigilated: false }
+		{ name: 'Assignment 1', weight: 25.0, mark: 100.0, invigilated: false },
+		{ name: 'Assignment 2', weight: 25.0, mark: 100.0, invigilated: false }
 	];
+
+	let assessments: Assessment[] = intialGetLocalAssessments() || [];
 	let totalWeighted = totalWeightedMark(assessments);
 	let totalInvigilated = totalInvigilatedWeightedMark(assessments);
 	let total = totalWeight(assessments);
@@ -33,11 +35,21 @@
 			: top >= 0 && left >= 0 && bottom <= innerHeight && right <= innerWidth;
 	}
 
+	function intialGetLocalAssessments() {
+		if (browser) {
+			const localAssessments = sessionStorage.getItem('assessments') as string;
+			if (!localAssessments) {
+				return DEMO_ASSESSMENTS;
+			}
+		}
+		return getLocalAssessments();
+	}
+
 	function getLocalAssessments() {
 		try {
-			const localAssessments = localStorage.getItem('assessments') as string;
+			const localAssessments = sessionStorage.getItem('assessments') as string;
 			const jsonAssessments = JSON.parse(localAssessments) as Assessment[];
-			const { valids, errors } = validateAssessments(jsonAssessments);
+			let { valids } = validateAssessments(jsonAssessments);
 			return valids;
 		} catch {
 			return null;
@@ -45,7 +57,7 @@
 	}
 
 	function setLocalAssessments(assessments: Assessment[]) {
-		if (browser) localStorage.setItem('assessments', JSON.stringify(assessments));
+		if (browser) sessionStorage.setItem('assessments', JSON.stringify(assessments));
 	}
 
 	function handleCalculate(scroll = false) {
@@ -123,19 +135,44 @@
 				<div
 					class="flex flex-col md:flex-row md:items-center p-4 gap-x-3 justify-between text-lg hover:shadow-md group rounded-md ring-1 ring-slate-200 bg-white dark:bg-gray-800 shadow-sm"
 				>
-					<input
-						bind:value={assessment.name}
-						on:keyup={(e) => handleKeyPress(e)}
-						placeholder="Assessment Name"
-						class=" font-bold outline-none text-inherit bg-inherit w-48"
-					/>
+					<div class="flex flex-row justify-between">
+						<input
+							bind:value={assessment.name}
+							on:keyup={(e) => handleKeyPress(e)}
+							on:focusout={() => handleCalculate()}
+							placeholder="Assessment Name"
+							class=" font-bold outline-none text-inherit bg-inherit w-48"
+						/>
+
+						{#if invalids.includes(i)}
+							<div
+								class="font-medium text-red-500 dark:text-red-400 md:hidden text-inherit bg-inherit hover:underline"
+							>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="w-6 h-6"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
+									/>
+								</svg>
+							</div>
+						{/if}
+					</div>
 
 					<label>
 						Weight:
 						<input
 							bind:value={assessment.weight}
 							on:keyup={(e) => handleKeyPress(e)}
-							class="outline-none w-32 text-inherit bg-inherit"
+							on:focusout={() => handleCalculate()}
+							class="outline-none w-32 text-inherit bg-inherit border-b border-dashed"
 						/>
 					</label>
 					<label>
@@ -143,23 +180,18 @@
 						<input
 							bind:value={assessment.mark}
 							on:keyup={(e) => handleKeyPress(e)}
-							class="outline-none w-32 text-inherit bg-inherit"
+							on:focusout={() => handleCalculate()}
+							class="outline-none w-32 text-inherit bg-inherit border-b border-dashed"
 						/>
 					</label>
 					<label class="w-32">
 						Invigilated:
 						<input type="checkbox" bind:checked={assessment.invigilated} class="outline-none" />
 					</label>
-					<div class="flex flex-wrap">
-						<div>
-							<span class="font-bold">{(weightedMark(assessment) || 0).toFixed(2)}%&nbsp;</span>
-						</div>
-						<div>of {total.toFixed(2)}%</div>
-					</div>
 
 					{#if invalids.includes(i)}
 						<div
-							class="font-medium md:flex md:justify-between text-inherit bg-inherit text-red-500 dark:text-red-400 hover:underline opacity-100 group-hover:hidden hidden"
+							class="font-medium md:flex md:justify-end md:flex-grow text-inherit bg-inherit text-red-500 dark:text-red-400 hover:underline opacity-100 group-hover:hidden hidden"
 						>
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
@@ -176,10 +208,17 @@
 								/>
 							</svg>
 						</div>
+					{:else}
+						<div class="md:flex md:flex-wrap md:flex-grow md:justify-end group-hover:hidden hidden">
+							<div>
+								<span class="font-bold">{(weightedMark(assessment) || 0).toFixed(2)}%&nbsp;</span>
+							</div>
+							<div>of {total.toFixed(2)}%</div>
+						</div>
 					{/if}
 					<button
 						on:click={() => handleRemove(i)}
-						class="font-medium hidden justify-end md:justify-between text-inherit bg-inherit hover:underline md:hidden md:group-hover:flex"
+						class="font-medium hidden justify-end md:justify-end md:flex-grow text-inherit bg-inherit hover:underline md:hidden md:group-hover:flex"
 						><svg
 							xmlns="http://www.w3.org/2000/svg"
 							fill="none"
@@ -214,26 +253,6 @@
 								/>
 							</svg>
 						</button>
-						{#if invalids.includes(i)}
-							<div
-								class="font-medium text-red-500 dark:text-red-400 flex md:hidden text-inherit bg-inherit hover:underline"
-							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="1.5"
-									stroke="currentColor"
-									class="w-6 h-6"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
-									/>
-								</svg>
-							</div>
-						{/if}
 					</div>
 				</div>
 			{/each}
